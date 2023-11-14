87a49d6ca9b2e7ba164cf890c2800bfc36aa603a
Sandesh <112424645+Sandy177000@users.noreply.github.com>
2023-11-14 12:59:38 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,7 @@ class A {
 	}
 class B extends A{
 	     public String meth(){
-	        return "Method is overridden in Extendend class B";
+	        return "Method is overridden in Extended class B";
 	    }
 }
 
